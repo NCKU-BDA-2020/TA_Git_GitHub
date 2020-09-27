921e598c87dacd17b42ecb4bdab762331174f699
CY <cyclonelee24@gmail.com>
2020-09-27 17:30:38 +0800

    Finish PPT

--- conflict_example.py
+++ conflict_example.py
@@ -1,10 +1,13 @@
 def example_func():
-    print('TA Class III')
+    print('Welcome to TA Class III')
     print('This is an example to show how to fix a conflict.')
 	
-for i in range(1):
+while(True):
+    name = input('Please enter your name：')
+    print('Hi~ ', name)
     example_func()
-	
+    print('\n')
+
 # testing revert
 # testing checkout
 # testing checkout2
